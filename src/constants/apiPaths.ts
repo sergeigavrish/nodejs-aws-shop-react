9cb0d796f4f0563f6b6a7378d9b4f06acee00f9b
Sergei Gavrish <sd.gavrish@gmail.com>
2025-04-14 13:10:41 +0200

    task-09: update api path

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,10 @@
 const API_PATHS = {
   product: "https://9fz5c7g8ei.execute-api.eu-west-1.amazonaws.com/dev",
-  order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  order:
+    "http://sergeigavrish-cart-api-dev.eu-west-1.elasticbeanstalk.com/api/profile/cart",
   import: "https://8djqxdyrzl.execute-api.eu-west-1.amazonaws.com/dev",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  cart: "http://sergeigavrish-cart-api-dev.eu-west-1.elasticbeanstalk.com/api",
 };
 
 export default API_PATHS;
