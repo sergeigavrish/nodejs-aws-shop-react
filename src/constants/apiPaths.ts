b5ae30a7f60f78cc9136ca8bc45e06cfa5b7bc84
Sergei Gavrish <sd.gavrish@gmail.com>
2025-04-15 04:21:29 +0200

    task-09: update api paths to cloudfront

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,10 +1,9 @@
 const API_PATHS = {
   product: "https://9fz5c7g8ei.execute-api.eu-west-1.amazonaws.com/dev",
-  order:
-    "http://sergeigavrish-cart-api-dev.eu-west-1.elasticbeanstalk.com/api/profile/cart",
+  order: "https://d1llsm8eh4cu70.cloudfront.net/api/profile/cart",
   import: "https://8djqxdyrzl.execute-api.eu-west-1.amazonaws.com/dev",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "http://sergeigavrish-cart-api-dev.eu-west-1.elasticbeanstalk.com/api",
+  cart: "https://d1llsm8eh4cu70.cloudfront.net/api",
 };
 
 export default API_PATHS;
